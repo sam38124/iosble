7bcf27c2e24800566e3854c5a4091ecfb1323da8
wangjianzhi <sam38124@gmail.com>
2019-04-16 12:05:04 +0800

    Initial Commit

---
+++ hh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hh
+//
+//  Created by 王建智 on 2019/4/16.
+//  Copyright © 2019 王建智. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
